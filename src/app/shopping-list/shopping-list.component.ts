73ab4f6956a0178dcfae177d3aa205601cf3e935
YY <yaroslavy.yakymenko@globallogic.com>
2017-07-29 12:39:57 +0300

    ingredients setup

--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {Ingredient} from '../shared/ingredient.model';
 
 @Component({
   selector: 'app-shopping-list',
@@ -6,8 +7,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./shopping-list.component.css']
 })
 export class ShoppingListComponent implements OnInit {
+  ingredients: Ingredient[] = [
+    new Ingredient('Apples', 2),
+    new Ingredient('Onion', 3),
+    new Ingredient('Garlic', 0.5),
+  ];
 
-  constructor() { }
+  constructor() {
+  }
 
   ngOnInit() {
   }
