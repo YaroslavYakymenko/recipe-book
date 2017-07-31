6f2ab90d07cab6140864eb5694778a7dc9c5fd90
YY <yaroslavy.yakymenko@globallogic.com>
2017-07-31 13:59:32 +0300

    added navigation and and modified recipe details div

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,4 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, EventEmitter, OnInit, Output} from '@angular/core';
 import {Recipe} from '../recipe.model';
 
 @Component({
@@ -7,9 +7,10 @@ import {Recipe} from '../recipe.model';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+  @Output() recipeWasSelected = new EventEmitter<Recipe>();
   recipes: Recipe[] = [
     new Recipe('Name of the recipe', 'Dummy description', 'http://media2.sailusfood.com/wp-content/uploads/2016/03/recipe-of-momos.jpg'),
-    new Recipe('Dummy recipe', 'Very good one', 'http://media2.sailusfood.com/wp-content/uploads/2016/03/recipe-of-momos.jpg')
+    new Recipe('Dummy recipe', 'Very good one', 'https://goo.gl/KJYifK')
   ];
 
   constructor() {
@@ -18,4 +19,7 @@ export class RecipeListComponent implements OnInit {
   ngOnInit() {
   }
 
+  onRecipeSelected(selectedRecipe: Recipe) {
+    this.recipeWasSelected.emit(selectedRecipe);
+  }
 }
